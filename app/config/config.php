8be74d229e25bb85a99ca5975c1ab76dce38ee36
AbdelillahElasri1 <elasriabdelilah68@gmail.com>
2022-12-10 00:00:02 +0100

    livrable 2 de brief 5

---
+++ app/config/config.php
@@ -0,0 +1,14 @@
+<?php
+  // DB Params
+  define("DB_HOST", "localhost");
+  define("DB_USER", "root");
+  define("DB_PASS", "123456");
+  define("DB_NAME", "traversymvc");
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/shareposts');
+  // Site Name
+  define('SITENAME', 'SharePosts');
+  
